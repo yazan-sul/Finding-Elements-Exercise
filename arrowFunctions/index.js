1a71ba2eda829ee74669ab88021b0a6ad5bb1d8c
yazan-sul <yazan-sul@gmail.com>
2025-07-27 17:23:49 +0300

    arrow functions

---
+++ arrowFunctions/index.js
@@ -0,0 +1,10 @@
+let divide = (a, b) =>{
+    return a / b;
+}
+let whisper = (str) =>{
+    console.log(str.toLowerCase());
+}
+let shorterThan = (array1, array2) =>{
+    if(array1.length < array2.length) return true;
+    return false;
+}
