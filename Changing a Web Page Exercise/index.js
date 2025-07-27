c4000ffee8e1309fb70b288e08001759df05d3b4
yazan-sul <yazan-sul@gmail.com>
2025-07-27 16:17:11 +0300

    third exercise solution

--- Changing a Web Page Exercise/index.js
+++ Changing a Web Page Exercise/index.js
@@ -10,3 +10,4 @@ document.getElementById("p1-symbol").textContent = p2Symbol;
 document.getElementById("p2-symbol").textContent = p1Symbol;
 
 document.querySelector('header h2').append("and love");
+
