7186e8fb408d6d48041b2704b2d243c96adf6614
yazan-sul <yazan-sul@gmail.com>
2025-07-27 15:13:32 +0300

    Solution for finding elements exercise

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const p = document.getElementsByTagName("p");  
+for (let i = 0; i < p.length; i++) {
+    console.log(p[i].textContent); 
+}
+
+const elements = document.querySelectorAll("*");
+const targetX = Array.from(elements).find(el => el.textContent.trim() === "X");
+console.log(targetX);
+
+const numberOfSquares = document.querySelectorAll(".square").length
+
+const target = Array.from(elements).find(el => el.textContent.trim() === "A game You Know");
+console.log(target);
