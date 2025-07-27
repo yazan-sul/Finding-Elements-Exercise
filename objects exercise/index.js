d45783061758e9f9522e4f4351943bdf2e47c9bf
yazan-sul <yazan-sul@gmail.com>
2025-07-27 17:02:47 +0300

    object exercises

---
+++ objects exercise/index.js
@@ -0,0 +1,17 @@
+const spices = [
+    {name: "Emma", nickname: "Baby"},
+    {name: "Geri", nickname: "Ginger"},
+    {name: "Mel B", nickname: "Scary"},
+    {name: "Mel c", nickname: "sporty"},
+    {name: "Victoria", nickname: "Posh"}
+]
+const spiceGirls = {
+    albums: ["spice", "spiceworld", "Forever"],
+    motto: "Girl Power",
+    members: spices
+}
+
+console.log(spiceGirls.motto);
+console.log(spiceGirls.members[1]);
+console.log(spiceGirls.albums[1]);
+console.log(spiceGirls.members[4]);
