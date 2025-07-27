102f5a26b0addec02adfa107bd83945693413312
yazan-sul <yazan-sul@gmail.com>
2025-07-27 16:48:18 +0300

    muatable & immuatable exercise

---
+++ Mutable & Immutable Data Exercise/index.js
@@ -0,0 +1,11 @@
+let numbers1 = [1, 2, 3];
+let result = numbers1.push(4);
+console.log(result); 
+console.log("------")
+let numbers2 = [1, 2, 3];
+let result2 = numbers1.concat([4]);
+console.log(result2);
+// output
+// 4
+// ------
+// [ 1, 2, 3, 4, 4 ]
