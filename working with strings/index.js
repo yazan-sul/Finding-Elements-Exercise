2ebe2ca2ef2cb6ba4689ce58522868bda4e609fc
yazan-sul <yazan-sul@gmail.com>
2025-07-27 16:30:33 +0300

    working with strings

---
+++ working with strings/index.js
@@ -0,0 +1,6 @@
+document.querySelector("#p1-name").textContent = document.querySelector("#p1-name").textContent + " Sulaiman";
+console.log(document.title.startsWith("JavaScript"));
+console.log(document.title.indexOf("T"));
+const heading = document.querySelector("header h1").textContent.toUpperCase();
+console.log(heading);
+
